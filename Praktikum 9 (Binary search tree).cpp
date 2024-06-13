a844afc674168b591f3e8fc38a888ef69091a113
gibranrafii <gibranrafi21@gmail.com>
2024-06-13 11:51:44 +0700

    Menyelesaikan int main

--- Praktikum 9 (Binary search tree).cpp
+++ Praktikum 9 (Binary search tree).cpp
@@ -119,5 +119,56 @@ public:
 
 int main()
 {
-   
+	BinaryTree x;
+	while (true)
+	{
+		cout << "\nMenu" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. Perform inorder traversal" << endl;
+		cout << "3. perform preorder traversal" << endl;
+		cout << "4. perform postorder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+
+		char ch;
+		cin >> ch;
+		cout << endl;
+
+		switch (ch)
+		{
+		case'1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			x.insert(word);
+			break;
+		}
+		case'2':
+		{
+			x.inorder(x.ROOT);
+			break;
+		}
+		case'3':
+		{
+			x.preorder(x.ROOT);
+			break;
+		}
+		case'4':
+		{
+			x.postorder(x.ROOT);
+			break;
+		}
+		case '5':
+		{
+			return 0;
+		}
+		default :
+		{
+			cout << "Invalid choice. Try again." << endl;
+			break;
+		}
+		}
+	}
+	return 0;
 }
